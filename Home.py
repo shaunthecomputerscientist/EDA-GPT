75fa71c8ddd0e857b0cdca8625f7447d93938a39
Shaunak Roy <mrpolymathematica@gmail.com>
2024-06-14 13:13:26 +0530

    commit changes

--- Home.py
+++ Home.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3']=sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3']=sys.modules.pop('pysqlite3')
 from pages.src.Tools.secrets import initialize_secrets, initialize_states
 def home():
 
